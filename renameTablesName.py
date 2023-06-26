d031e71f6f851575d7726c8ecd178eb86b1e2ea8
Nikita Arinchehin <narincehin@gmail.com>
2023-06-26 13:48:32 +0300

    read all files in \models

--- renameTablesName.py
+++ renameTablesName.py
@@ -1,5 +1,15 @@
-from pathlib import Path
-
+from pathlib import Path;
+import os
+# from openpyxl import Workbook;
+import pandas as pd;
+# dataMsSQL = pd.read_excel('C:\excelSolution\Tablitsy-SootvetstviaImen.xlsx', sheet_name='Таблицы-Соответствия' , usecols='D');
+dataPostgre = pd.read_excel('C:\excelSolution\Tablitsy-SootvetstviaImen.xlsx', sheet_name='Таблицы-Соответствия' , usecols='D , E');
 p = Path("C:\excelSolution\models")
 for x in p.rglob("*"):
-    print(x)
+    open_file = open(x,'r')
+    read_file = open_file.read()
+    print(read_file)
+    # for idx , row in dataPostgre.iterrows():
+    #     rowMSSQL = row['Таблица MS SQL'];
+    #     rowPostgreSQL = row['Таблица PostgreSQL']
+    #     print(rowPostgreSQL)

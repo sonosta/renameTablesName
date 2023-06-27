3f512ed04b5a5780d0841976807a2fee6b446793
Nikita Arinchehin <narincehin@gmail.com>
2023-06-27 15:19:29 +0300

    rename tables

--- renameTablesName.py
+++ renameTablesName.py
@@ -1,15 +1,23 @@
 from pathlib import Path;
-import os
-# from openpyxl import Workbook;
 import pandas as pd;
-# dataMsSQL = pd.read_excel('C:\excelSolution\Tablitsy-SootvetstviaImen.xlsx', sheet_name='Таблицы-Соответствия' , usecols='D');
-dataPostgre = pd.read_excel('C:\excelSolution\Tablitsy-SootvetstviaImen.xlsx', sheet_name='Таблицы-Соответствия' , usecols='D , E');
+import re;
+dataExcel = pd.read_excel('C:\excelSolution\Tablitsy-SootvetstviaImen.xlsx', sheet_name='Таблицы-Соответствия' , usecols='D , E');
 p = Path("C:\excelSolution\models")
 for x in p.rglob("*"):
-    open_file = open(x,'r')
-    read_file = open_file.read()
-    print(read_file)
-    # for idx , row in dataPostgre.iterrows():
-    #     rowMSSQL = row['Таблица MS SQL'];
-    #     rowPostgreSQL = row['Таблица PostgreSQL']
-    #     print(rowPostgreSQL)
+    try:
+        f = open (x , 'r' , encoding='utf-8')
+        old_data = f.read()
+        for idx , row in dataExcel.iterrows():
+            rowMSSQL = row['Таблица MS SQL']
+            rowPostgreSQL = row['Таблица PostgreSQL']
+            haveTableName = old_data.find(rowMSSQL)
+            if haveTableName != -1 and rowPostgreSQL != 0:
+                    file = open (x , 'w' , encoding='utf-8')
+                    # old_data = old_data.replace(rowMSSQL , rowPostgreSQL)
+                    old_data = re.sub(rowMSSQL , rowPostgreSQL , old_data)
+                    file.write(old_data)
+                    print('success')
+                    file.close()
+    except Exception as err:
+        #Ошибка может вылезать, если попытаемся прочитать папку с файлами
+        print(err)
